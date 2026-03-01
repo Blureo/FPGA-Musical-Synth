16ebb16d1154f614e512ab013cc32ebbf941fb7f
PKCubed <59457514+PKCubed@users.noreply.github.com>
2026-03-01 09:52:00 -0800

    Update nco.sv
    Added inputs and accumulator logic

--- circuitware/NCO/nco.sv
+++ circuitware/NCO/nco.sv
@@ -1,9 +1,27 @@
 module nco
+{
+    input logic rst
+    input logic master_clk,
+    input logic tone_clk,
+    input logic [31:0] accumulator_increment_value 
+};
+    // Waveform ROM
+    logic [15:0] waveform_rom [0:31];
 
-// Waveform ROM
-logic [15:0] waveform_rom [0:31];
+    // Waveform Slope ROM
+    logic [15:0] waveform_slope_rom [0:31];
 
-// Waveform Slope ROM
-logic [15:0] waveform_slope_rom [0:31];
+    // 32 bit accumulator
+    logic [31:0] accumulator_value;
+
+    always_ff @(posedge clk or negedge rst) begin
+        if (!rst_n) begin
+            // Reset the register to 0
+            accumulator_value <= 32'h0;
+        end else begin
+            accumulator_value <= accumulator_value + accumulator_increment_value;
+        end
+    end
+
 
 endmodule
