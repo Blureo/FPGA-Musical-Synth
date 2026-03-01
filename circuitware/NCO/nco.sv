29adf070b664d5d4f81d72e259ea9afd190020cb
PKCubed <59457514+PKCubed@users.noreply.github.com>
2026-03-01 13:00:27 -0800

    Update nco.sv
    Started to implement the linear interpolation logic, and added a comment explaining how we plan to prevent modifying register values in between partial serial transmissions causing problems.

--- circuitware/NCO/nco.sv
+++ circuitware/NCO/nco.sv
@@ -9,6 +9,10 @@ wave output we want.
 
 When the tone_frequency_calculator.sv module detects no keys are being pressed, it will give this
 module a high value on the nco_mute logic line. This will set the accumulator value back to 0.
+
+To ensure that the i2s_transmitter module doesn't get halfway through serially outputting before
+the sample gets changed by this module, the i2s_transmitter will locally save the sample to its
+own register, in the process delaying the output by one sample, but preventing this issue.
 */
 
 module nco
@@ -28,12 +32,20 @@ module nco
     // 32 bit accumulator
     logic [31:0] accumulator_value;
 
-    always_ff @(posedge clk or negedge rst) begin
+    logic [15:0] sample;
+    logic [15:0] slope;
+
+    always_comb @(posedge clk or negedge rst) begin
         if (!rst_n or nco_mute) begin
             // Reset the register to 0
             accumulator_value <= 32'h0;
+            sample <= 16'h0;
         end else begin
             accumulator_value <= accumulator_value + accumulator_increment_value;
+            sample <= waveform_rom[accumulator_value[31:26]]; // Take 5 MSbs of the accumulator and get the value in the waveform rom at that address.
+            slope <= waveform_slope_rom[accumulator_value[31:26]]; // Take 5 MSbs of the accumulator and get the value in the waveform slope rom at that address.
+
+            // Here, we need to mulitply the slope by the least significant 27 bits of the accumulator value, then bitshift right by 27 bits. Then that value gets added to the sample.
         end
     end
 
